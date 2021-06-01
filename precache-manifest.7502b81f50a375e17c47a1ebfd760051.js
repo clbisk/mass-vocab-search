1d0cd1b9d752b72516df7c51a82494bde635b854
Cecilia Bisk <cecilia.bisk.17@cnu.edu>
2021-06-01 19:18:14 -0400

    Updates

---
+++ precache-manifest.7502b81f50a375e17c47a1ebfd760051.js
@@ -0,0 +1,34 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "14940f2baf71fc6a51f119205bf6b70f",
+    "url": "/mass-vocab-search/index.html"
+  },
+  {
+    "revision": "b789900e5c75bae5fa0e",
+    "url": "/mass-vocab-search/static/css/2.c465c46e.chunk.css"
+  },
+  {
+    "revision": "94deb308f49f38f04ca3",
+    "url": "/mass-vocab-search/static/css/main.73ee7956.chunk.css"
+  },
+  {
+    "revision": "b789900e5c75bae5fa0e",
+    "url": "/mass-vocab-search/static/js/2.59b0744b.chunk.js"
+  },
+  {
+    "revision": "434eeea35bf11e3d22931f972c8fbb4c",
+    "url": "/mass-vocab-search/static/js/2.59b0744b.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "94deb308f49f38f04ca3",
+    "url": "/mass-vocab-search/static/js/main.9f66c779.chunk.js"
+  },
+  {
+    "revision": "beb2be39e213d3ac8d4f",
+    "url": "/mass-vocab-search/static/js/runtime-main.ead55c9b.js"
+  },
+  {
+    "revision": "1cedb6e919bfed6a2c1ec00b5d8ee620",
+    "url": "/mass-vocab-search/static/media/UploadIcon.1cedb6e9.svg"
+  }
+]);
